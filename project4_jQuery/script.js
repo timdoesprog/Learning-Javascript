359a8fc6d5f79cd4a19fb381aade78f458b8b637
Tim Betz <writingtumblr@gmx.de>
2017-04-17 12:31:53 +0200

    jQuery Introduction
    Learned about jQuery selectors and basic event handling with the on()
    method.

---
+++ project4_jQuery/script.js
@@ -0,0 +1,30 @@
+// JQUERY SELECTORS
+
+// $('li').on('click', function () {
+//     $(this).text('clicked').delay(800).fadeOut(400);
+// });
+//
+// // Element Selectors
+//     // $('li')
+//     // $('img')
+//     // $('a')
+//
+// // Descended Selectors
+//     // $('li a')
+//
+// // Class and Id Selectors
+//     // $('.container')
+//     // $('#profile-picture')
+//
+// // Attribute Selector
+//     // $([name='newsletter'])
+//
+// $('button').on('click', function () {
+//     $('a').attr('href', 'http://youtube.com');
+// });
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$('body').click(function () {
+    console.log('clicked');
+});
